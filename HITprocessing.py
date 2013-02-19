2156a8a6e44712d2b161a3bfe89f458b1a412147
Thomas Selhorst <Thomas.Selhorst@gmail.com>
2013-02-19 18:05:24 +0100

    modified if clauses: added continue to if clauses to enhance speed of data processing

--- HITprocessing.py
+++ HITprocessing.py
@@ -27,24 +27,16 @@ def HIT2DiGraph(G, fnEdgeList="", startdate="01.01.1999", enddate="31.12.2011", 
         volume = row[3]
         date=datetime.strptime(d,"%d%b%Y").date()
         #
-        if(bnr_vb[:3]=="276" and bnr_nb[:3]=="276"):
-            domesticTrade = True
+        # filter the data according ot the State, the time window and the Federal state
         #
-        #print "Kreis ist: "+bnr_vb[3:8]
-
-	    if(state == ""):
-	        inKreis = True
-        else:
-            if(bnr_vb[3:8]==state):
-                inKreis = True
-            
-        if(sdate <= date and date < edate):
-            inTimeWindow=True
+        if(bnr_vb[:3]!="276" or bnr_nb[:3]!="276"): continue
         #
-        if(domesticTrade==True and inTimeWindow==True and int(volume)>0 and inKreis==True):
-            G.add_edge(bnr_vb, bnr_nb)
-            #print bnr_nb, bnr_vb
-
+        if(state !="" and bnr_vb[3:8]!=state): continue
+        #
+        if(date < sdate and date >= edate): continue
+        #
+        G.add_edge(bnr_vb, bnr_nb)
+        #
     print "Zeitraum "+startdate+" bis "+enddate
     print "G has nodes "+str(G.number_of_nodes())
     print "G has edges "+str(G.number_of_edges())
@@ -52,9 +44,9 @@ def HIT2DiGraph(G, fnEdgeList="", startdate="01.01.1999", enddate="31.12.2011", 
 
 if __name__=="__main__":
     G = nx.DiGraph()
-    #HIT2DiGraph(G,"/Users/tselhorst/SNA Cattle/r_beweg_sample.csv","01.11.2001","01.12.2001")
+    HIT2DiGraph(G,"/Users/tselhorst/SNA Cattle/r_beweg_sample.csv","01.04.2001","01.12.2001")
     print "Starting HIT2DiGraph "
-    HIT2DiGraph(G,"/Users/tselhorst/SNA Cattle/r_beweg.csv","01.09.2009","01.12.2009") # 8 Jahre in der Untersuchung
+    #HIT2DiGraph(G,"/Users/tselhorst/SNA Cattle/r_beweg.csv","01.09.2009","01.12.2009") # 8 Jahre in der Untersuchung
     #HIT2DiGraph(G,"/Users/tselhorst/Forschungsprojekte/SliLeBAT/Handelnetz Rinder/Daten/sanHIT_2002-2009dg.csv","01.01.2009","01.11.2009") # 8 Jahre in der Untersuchung
     for e in G.edges():
         print str(e[1])+";"+str(e[0])
