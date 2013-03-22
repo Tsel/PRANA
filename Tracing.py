5d1766d626f589e3515d0fceed4154f0c1c6fb30
Thomas Selhorst <Thomas.Selhorst@gmail.com>
2013-03-22 17:44:18 +0100

    new bfs algorithm
    Bug: resulting network not correct
    Bug: Source points to source on the same say 1.9?

--- Tracing.py
+++ Tracing.py
@@ -9,9 +9,9 @@ from collections import defaultdict
 
 outcomponent = set()
 
-def my_bfs_edges(G, source):
-    """ Adapted Producure to produce edges in breath-first search starting at source. """
-    visited = set([source])
+def my_bfs_edges_m(G, source):
+    """ Adapted procedure to produce edges in breadth-first search statring at source. """
+    """ set visited is not needed, because all necessary information is stored at the node level """
     stack = [(source, iter(G[source]))]
     while stack:
         parent, children = stack[0]
@@ -20,25 +20,54 @@ def my_bfs_edges(G, source):
             e = (parent, child)
             sDoC = G.get_edge_data(*e)["trade_date"]
             DoC  = datetime.strptime(sDoC.title(), "%d%b%Y").date()
-
-            if child not in visited and DoC > G.node[parent]['doi']:
-                G.node[child]['k']=G.node[parent][k]+1
-                G.node[child]['doi']=DoC
-                G.node[child]['ttc'] = G.node[parent]['ttc'] + (DoC - G.node[parent]['doi']) 
+            if ('doi' not in G.node[child] or DoC < G.node[child]['doi'] or (DoC == G.node[child]['doi'] and G.node[child]['Infector'] != G.node[parent]['Infector'])) and (DoC > G.node[parent]['doi']):
+                G.node[child]['k'] = G.node[parent]['k']+1
+                G.node[child]['doi'] = DoC
+                G.node[child]['ttc'] = G.node[parent]['ttc'] + (DoC - G.node[parent]['doi'])
+                G.node[child]['Infector'] = G.node[parent]['Infector']
                 yield parent, child
-                visited.add(child)
                 stack.append((child, iter(G[child])))
-            elif child in visited and DoC < G.node[child]['doi'] and DoC > G.node[parent]['doi']:
+        except StopIteration:
+            stack.pop(0)
+
+
+def my_bfs_edges(G, source):
+    """ Adapted Producure to produce edges in breadth-first search starting at source. """
+    visited = set([source])
+    stack = [(source, iter(G[source]))]
+    while stack:
+        parent, children = stack[0]
+        try:
+            child = next(children)
+            #print parent, child
+            e = (parent, child)
+            sDoC = G.get_edge_data(*e)["trade_date"]
+            DoC  = datetime.strptime(sDoC.title(), "%d%b%Y").date()
+
+            if child not in visited and DoC >= G.node[parent]['doi']:
+                if 'doi' not in G.node[child] or DoC < G.node[child]['doi']:
+                    G.node[child]['k']=G.node[parent]['k']+1
+                    G.node[child]['doi']=DoC
+                    G.node[child]['ttc'] = G.node[parent]['ttc'] + (DoC - G.node[parent]['doi']) 
+                    G.node[child]['toc'] = DoC
+                    G.node[child]['Infector'] = G.node[parent]['Infector']
+                    yield parent, child
+                    visited.add(child)
+                    stack.append((child, iter(G[child])))
+            elif child in visited and DoC < G.node[child]['doi'] and DoC >= G.node[parent]['doi']:
                 G.node[child]['ttc'] = G.node[parent]['ttc'] + (DoC - G.node[parent]['doi']) 
                 G.node[child]['doi'] = DoC
-                G.node[child]['k']=G.node[parent][k]+1
+                G.node[child]['k']=G.node[parent]['k']+1
+                G.node[child]['toc'] = DoC
+                G.node[child]['Infector']=G.node[parent]['Infector']
                 yield parent, child
+                #stack.append( (child, iter(G[child])) )
         except StopIteration:
             stack.pop(0)
 
 def my_bfs_successors(G, source):
     d = defaultdict(list)
-    for s,t in my_bfs_edges(G, source):
+    for s,t in my_bfs_edges_m(G, source):
         d[s].append(t)
     return d
 
@@ -156,12 +185,12 @@ def OC2file(G, oc, fn=""):
          
 if __name__ == "__main__":
     print "Create network G"
-    G = nx.read_edgelist("/Users/tselhorst/Forschungsprojekte/Rinder/Handelsdaten/virtualTrade.csv",delimiter=";",create_using=nx.DiGraph(), nodetype=str, data=(('trade_date',str),))
+    G = nx.read_edgelist("/Users/tselhorst/Forschungsprojekte/Rinder/Handelsdaten/Cattle_Trade_01092009_31112009.csv",delimiter=";",create_using=nx.DiGraph(), nodetype=str, data=(('trade_date',str),))
     print "G has nodes "+str(G.number_of_nodes())
     #SCC = nx.strongly_connected_components(G)
     #lSCC = SCC[0]
     print "Read start nodes for out component"
-    startnodes = getStartNodes(fn="/Users/tselhorst/Forschungsprojekte/Rinder/Handelsdaten/virtualTrade_sources.csv")
+    startnodes = getStartNodes(fn="/Users/tselhorst/Forschungsprojekte/Rinder/Handelsdaten/Sources_05554_rs2.csv")
     start_date = "01Sep2009"
     #for n in startnodes:
     #    print n, G[n]
@@ -179,38 +208,81 @@ if __name__ == "__main__":
         G.node[node]['ttc']=sd-sd   # time to contact [days]
         G.node[node]['k']=0         # infection sequence [0: farms at start]
         G.node[node]['doi']=sd      # date of infection
-        G.node[node]['ID']=node     # ID of initial node
+        G.node[node]['Infector']=node     # ID of initial node
     #    outcomponent.add(node)
     #print str(noNodesINlSCC) + " of "+str(len(startnodes)) + " in lSCC"
 
     #sys.setrecursionlimit(9000)
     ChronoGraph = nx.DiGraph()
+    distinctNodes = set()
     for node in startnodes:
-        print "-----------------"
-        print "Startnode ", node
+        counter=0
+        ccounter = 0
+        #print "-----------------"
+        #print "Startnode ", node
         el = my_bfs_successors(G, node)
+        #print "returned edge list ", el
+        #print "     Edge, trade date, k, ttc, doi, Infector"
         for s in el:
+            distinctNodes.add(s)
             for t in el[s]:
+                distinctNodes.add(t)
                 edge = (s,t)
                 sedgeDoC = (G.get_edge_data(*edge)['trade_date'])
-                t_in_e = ChronoGraph.in_edges(t)
-                if(len(t_in_e) == 0):
-                    ChronoGraph.add_edge(s,t, trade_date=sedgeDoC)
-                else:
-                    edgeDoC = datetime.strptime(G.get_edge_data(*edge)['trade_date'].title(),"%d%b%Y").date()
-                    #print s, t, edgeDoC
-                    # in edges von t suchen
-                    for e in ChronoGraph.in_edges(t):
-                        t_inDoC = datetime.strptime(G.get_edge_data(*e)['trade_date'].title(),"%d%b%Y").date()
-                        #print "tested edge ", t_in_e, t_inDoC, edgeDoC
-                        if(t_inDoC > edgeDoC):
-                            print "edge deleted ",e
+                #print "Edge to add ", edge
+                edgeDoC = datetime.strptime(G.get_edge_data(*edge)['trade_date'].title(),"%d%b%Y").date()
+                if ChronoGraph.has_node(t):
+                    plt = ChronoGraph.predecessors(t)
+                    for p in plt:
+                        e = (p,t)
+                        tedgeDoC = datetime.strptime(ChronoGraph.get_edge_data(*e)['trade_date'].title(),"%d%b%Y").date()
+                        if edgeDoC < tedgeDoC:
                             ChronoGraph.remove_edge(*e)
-                            print "edge added   ", s, t
-                            ChronoGraph.add_edge(s,t,trade_date=sedgeDoC)
+                    if not ChronoGraph.predecessors(t):
+                        ChronoGraph.add_edge(s, t, trade_date=sedgeDoC)
 
-    #print ChronoGraph.edges()
-    print "Graph has nodes ", nx.number_of_nodes(ChronoGraph)
+                else:
+                    ChronoGraph.add_edge(s,t, trade_date=sedgeDoC)
+
+                    #print s, t, G.node[t]['doi'], G.node[t]['k'], G.node[t]['ttc'].days, G.node[t]['Infector']
+        #print "In degree of start node  ", G.in_degree(node)
+        #print "Out degree of start node ", G.out_degree(node)
+                #t_in_e = ChronoGraph.in_edges(t)
+                #if(len(t_in_e) == 0):
+                #    ChronoGraph.add_edge(s,t, trade_date=sedgeDoC)
+                #else:
+                #    edgeDoC = datetime.strptime(G.get_edge_data(*edge)['trade_date'].title(),"%d%b%Y").date()
+                #    #print s, t, edgeDoC
+                #    # in edges von t suchen
+                #    for e in ChronoGraph.in_edges(t):
+                #        t_inDoC = datetime.strptime(G.get_edge_data(*e)['trade_date'].title(),"%d%b%Y").date()
+                #        #print "tested edge ", t_in_e, t_inDoC, edgeDoC
+                #        if(t_inDoC > edgeDoC):
+                #            #print "edge deleted ",e
+                #            ChronoGraph.remove_edge(*e)
+                #            #print "edge added   ", s, t
+                #            ChronoGraph.add_edge(s,t,trade_date=sedgeDoC)
+
+ #   print ChronoGraph.edges()
+    #print "EdgeList original graph"
+    #for e in G.edges_iter():
+    #    print e
+
+    #print "Edgelist of ChronoGraph"
+    #for e in ChronoGraph.edges_iter():
+    #    print e
+
+    #print "Node information: "
+    #for n in ChronoGraph.nodes_iter():
+    #    print n, G.node[n]['ttc'].days, G.node[n]['k'], G.node[n]['doi'], G.node[n]['Infector']
+
+    #print "Graph has nodes ", nx.number_of_nodes(ChronoGraph)
+    #print "Graph has edges ", nx.number_of_edges(ChronoGraph)
+    #print "distinct nodes in set ", len(distinctNodes)
+    for n in distinctNodes:
+        print n
+
+
 
 
          
